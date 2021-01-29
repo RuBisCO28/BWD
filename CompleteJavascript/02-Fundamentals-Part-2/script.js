d12c4d1d9cbe2dcef6f6763dec8419d304630dbb
RuBisCO28 <54470624+RuBisCO28@users.noreply.github.com>
2021-01-29 23:52:55 +0900

    Complete Section3 Coding Challenge #2

--- CompleteJavascript/02-Fundamentals-Part-2/script.js
+++ CompleteJavascript/02-Fundamentals-Part-2/script.js
@@ -89,8 +89,6 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
 
-*/
-
 const calcAverage = (firstScore, secondScore, thirdScore) => {
     return (firstScore+secondScore+thirdScore) / 3;
 }
@@ -114,3 +112,83 @@ console.log(checkWinner(dolphinsAverageScoreFirst, koalasAverageScoreFirst));
 const dolphinsAverageScoreSecond = calcAverage(85,54,41);
 const koalasAverageScoreSecond = calcAverage(23,34,27);
 console.log(checkWinner(dolphinsAverageScoreSecond, koalasAverageScoreSecond));
+
+
+
+const friend1 = 'Micheal';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2000, 2020);
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice'];
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schemedtmann', 2037 -1991, friends];
+console.log(jonas);
+
+const calcAge = (birthYear) => {
+    return 2037 - birthYear;
+}
+const y = [1990, 1967, 2002, 2010, 2018];
+console.log(calcAge(y)); // NaN
+console.log(calcAge(y[0]));
+console.log(calcAge(y[1]));
+console.log(calcAge(y[years.length -1]));
+
+const ages = [calcAge(y[0]), calcAge(y[1]), calcAge(y[years.length -1])];
+console.log(ages);
+
+// Add elements
+const friends = ['Micheal', 'Steven', 'Peter'];
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+const popped = friends.pop(); // Last
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+// Search element
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+console.log(friends.includes(23));
+
+*/
+
+const calcTip = (bill) => {
+    if(50 <= bill && bill <= 300){
+        return bill * 0.15;
+    } else {
+        return bill * 0.2;
+    }
+}
+
+console.log(calcTip(100));
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+const total = [bills[0]+calcTip(bills[0]), bills[1]+calcTip(bills[1]), bills[2]+calcTip(bills[2])];
+console.log(total);
